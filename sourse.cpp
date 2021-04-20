474fc3611e507c385e0fdeef9621c6071708e76f
Kushagra Srivastava <suobset@gmail.com>
2021-04-20 03:32:48 -0400

    add teh sourse kernals

---
+++ sourse.cpp
@@ -0,0 +1,20 @@
+int main(){
+  char y;
+  cout<<"Press 2 to start compile";
+  cin>>y;
+  
+  if(y == "2"){
+    cout<<"Compile Lunix Source in poggers"<<endl;
+    for(int i = 0; i < 10; ++i){
+      cout<<"Lunix Compile Poggers "<<i * 10<<" % done \n";
+    }
+    cout<<"Lunix compile poggers done \ngo hack da mainframe now"<<endl;
+    cout<<"Download the mp3 software hack music, download more RAM, and thank Linyos Torovoltos to free you from Michaelsoft Binbows"<<endl;
+    cout<<"Linyos salutes you, comrade"<<endl;
+    return 0;
+  }
+  else{
+    cout<<"UNAUTHORISED ACCESS DETEKT!!! CALLING KGB AND ALERTING TOROVOLTOS NOW"<<endl;
+    return 0;
+  }
+}
